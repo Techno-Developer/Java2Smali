d8e1516273d8868b5035342d4b1f33c010bb531f
Techno-Developer <31841978+Techno-Developer@users.noreply.github.com>
2020-03-18 19:50:36 +0100

    Added command line options Added --version and --help

--- src/com/technodeveloper/java2smali/Main.java
+++ src/com/technodeveloper/java2smali/Main.java
@@ -8,7 +8,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
-    private static final String HELP_MESSAGE = "Usage:\n\tJava2Smali <JavaSourceFile>";
+    private static final String VERSION = "1.2";
+
+    private static final String HELP_MESSAGE = "Usage:" +
+            "\n\tJava2Smali <JavaSourceFile>" +
+            "\n\tJava2Smali --version" +
+            "\n\tJava2Smali --help";
     private static final String NOFILE_ERROR = "[E] $FILE$ does not exist.";
     private static final String JAVA_EXT = ".java";
     private static final String CLASS_EXT = ".class";
@@ -27,7 +32,16 @@ public class Main {
         if (args.length == 0) {
             print(HELP_MESSAGE);
         } else {
-            if (new File(args[0]).exists()) {
+            if (args[0].startsWith("--")) {
+                if (args[0].equals("--version") || args[0].equals("--v")) {
+                    print("Java2Smali v" + VERSION);
+                } else if (args[0].equals("--help") || args[0].equals("--h")) {
+                    print(HELP_MESSAGE);
+                } else {
+                    print("[E] Unknown command");
+                    print(HELP_MESSAGE);
+                }
+            } else if (new File(args[0]).exists()) {
                 convert(args[0]);
                 extractAndClean();
                 print("[I] Done.");
