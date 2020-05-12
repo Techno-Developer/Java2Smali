087b5836ac8b01a18bf283af94d540c7c672effa
Techno-Developer <31841978+Techno-Developer@users.noreply.github.com>
2020-05-12 13:24:06 +0200

    Major improvements Added ConversionManager class for a better conversion management. Added the ability to convert folders. Added option to disable optimization --no-optimize. Fixed error that doesn't allow to run from a separate folder Improved performance. Updated version to 1.3

--- src/com/technodeveloper/java2smali/Main.java
+++ src/com/technodeveloper/java2smali/Main.java
@@ -1,109 +1,121 @@
 package com.technodeveloper.java2smali;
 
-import com.technodeveloper.java2smali.util.ToolsManager;
+import com.technodeveloper.java2smali.util.ConversionManager;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Main {
-    private static final String VERSION = "1.2.1";
-
+    public static final String SEPARATOR = File.separator;
+    public static final String OUT_SMALI_FOLDER = "converted";
+    public static final String JAVA_EXT = ".java";
+    public static final String CLASS_EXT = ".class";
+    public static final String SMALI_EXT = ".smali";
+    private static final String VERSION = "1.3";
     private static final String HELP_MESSAGE = "Usage:" +
-            "\n\tJava2Smali <JavaSourceFile>" +
+            "\n\tJava2Smali [--no-optimize] <JavaSource / JavaSourcesDirectory>" +
             "\n\tJava2Smali --version" +
             "\n\tJava2Smali --help";
     private static final String NOFILE_ERROR = "[E] $FILE$ does not exist.";
-    private static final String JAVA_EXT = ".java";
-    private static final String CLASS_EXT = ".class";
-    private static final String SMALI_EXT = ".smali";
-
-    private String javaClassFileName;
-    private String smaliFileName;
-    private String dexFileName;
+    private File compiledJava;
 
     public static void main(String[] args) {
         new Main().init(args);
     }
 
+    public static void print(String s) {
+        System.out.println(s);
+    }
+
     private void init(String[] args) {
         if (args.length == 0) {
             print(HELP_MESSAGE);
+            System.exit(1);
         } else {
             if (args[0].startsWith("--")) {
-                if (args[0].equals("--version") || args[0].equals("--v")) {
-                    print("Java2Smali v" + VERSION);
-                } else if (args[0].equals("--help") || args[0].equals("--h")) {
-                    print(HELP_MESSAGE);
-                } else {
-                    print("[E] Unknown command");
-                    print(HELP_MESSAGE);
+                switch (args[0]) {
+                    case "--version":
+                    case "--v":
+                        print("Java2Smali v" + VERSION);
+                        break;
+                    case "--help":
+                    case "--h":
+                        print(HELP_MESSAGE);
+                        break;
+                    case "--no-optimize":
+                    case "--n":
+                        if (args.length > 1) convertAndClean(args[1], false);
+                        else print("[E] Missing file.");
+                        break;
+                    default:
+                        print("[E] Unknown command.");
+                        print(HELP_MESSAGE);
+                        break;
                 }
-            } else if (new File(args[0]).exists()) {
-                convert(args[0]);
-                extractAndClean();
-                print("[I] Done.");
             } else {
-                print(NOFILE_ERROR.replace("$FILE$", args[0]));
+                convertAndClean(args[0]);
             }
         }
     }
 
-    private void convert(String fileName) {
-        smaliFileName = fileName.replace(JAVA_EXT, SMALI_EXT);
-        javaClassFileName = fileName.replace(JAVA_EXT, CLASS_EXT);
-        dexFileName = "classes.dex";
+    private void convertAndClean(String providedPath) {
+        convertAndClean(providedPath, true);
+    }
+
+    private void convertAndClean(String providedPath, boolean optimize) {
+        String providedFormattedPath = formatPath(providedPath);
+        checkPath(providedFormattedPath);
+        File providedFile = new File(providedFormattedPath).getAbsoluteFile();
+
+        if (providedFile.exists()) {
+            // Convert to smali
+            convert(providedFile, optimize);
+
+            // Extract and clean
+            //print("[I] Extracting smali files...");
+            //ConversionManager.extractSmaliFiles(new File(providedFile.getParentFile().getAbsolutePath() + SEPARATOR + OUT_SMALI_FOLDER));
+            print("[I] Cleaning...");
+            ConversionManager.clean(providedFile.getParentFile(), compiledJava);
+            print("[I] Done.");
+        } else {
+            print(NOFILE_ERROR.replace("$FILE$", providedFormattedPath));
+        }
+    }
+
+    private void convert(File providedFile, boolean optimize) {
 
         print("[I] Compiling...");
-        String errors = ToolsManager.runJavac(fileName);
-        if (errors.contains("error")) {
-            print("[E] Error during the compilation of ".concat(fileName).concat("\n").concat(errors));
-            print("[E] Compilation failed, check errors above for more info.");
-            System.exit(1);
+        if (providedFile.isDirectory()) {
+            compiledJava = ConversionManager.compileJavaFolder(providedFile);
+        } else {
+            compiledJava = ConversionManager.compileJavaFile(providedFile);
         }
-        print("[I] Compiled successfully.");
 
         print("[I] Dexing...");
-        ToolsManager.runDx(new String[]{"--dex", "--output", dexFileName, javaClassFileName});
+        File dexFile;
+        if (providedFile.isDirectory()) {
+            dexFile = ConversionManager.dexClassFolder(compiledJava, optimize);
+        } else {
+            dexFile = ConversionManager.dexClassFile(compiledJava, optimize);
+        }
 
         print("[I] Running baksmali...");
-        ToolsManager.runBaksmali(new String[]{"d", dexFileName});
+        ConversionManager.baksmaliDex(dexFile);
     }
 
-    private void extractAndClean() {
-        print("[I] Copying smali source...");
-        File smaliFile = new File("out/".concat(smaliFileName));
+    private String formatPath(String path) {
+        String formattedPath = path;
+        if (path.endsWith("\"")) {
+            formattedPath = formattedPath.substring(0, formattedPath.length() - 1);
+        }
 
-        try {
-            File oldSmaliFile = new File(smaliFileName);
-            if (oldSmaliFile.exists()) {
-                print("[I] Deleting duplicate...");
-                deleteFile(oldSmaliFile);
-            }
-            Files.move(Paths.get(smaliFile.toURI()), Paths.get(smaliFileName));
-        } catch (IOException e) {
-            print("[E] Error during extraction.");
+        return formattedPath;
+    }
+
+    private void checkPath(String path) {
+        if (new File(path).isDirectory() && path.contains(" ")) {
+            print("[E] The path provided contains spaces.");
+            print("[I] Only the conversion of single files is available for paths that contain spaces.");
             System.exit(1);
         }
-
-        print("[I] Deleting junk files...");
-        deleteFile("out");
-        deleteFile(javaClassFileName);
-        deleteFile(dexFileName);
-    }
-
-    private static void deleteFile(String fileName) {
-        deleteFile(new File(fileName));
-    }
-
-    private static void deleteFile(File file) {
-        if (!file.delete()) {
-            print("[E] Cannot delete ".concat(file.getName()).concat("."));
-        }
-    }
-
-    public static void print(String s) {
-        System.out.println(s);
     }
 }
--- src/com/technodeveloper/java2smali/util/ToolsManager.java
+++ src/com/technodeveloper/java2smali/util/ToolsManager.java
@@ -1,36 +1,17 @@
 package com.technodeveloper.java2smali.util;
 
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
-import java.io.OutputStream;
+import com.technodeveloper.java2smali.Main;
+
+import java.io.File;
+
+import static com.technodeveloper.java2smali.Main.OUT_SMALI_FOLDER;
 
 public class ToolsManager {
-    public static String runJavac(String sourceFile) {
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-
-        OutputStream errors = new OutputStream() {
-            private final StringBuilder sb = new StringBuilder();
-
-            @Override
-            public void write(int b) {
-                this.sb.append((char) b);
-            }
-
-            @Override
-            public String toString() {
-                return this.sb.toString();
-            }
-        };
-
-        compiler.run(null, null, errors, "-source", "1.8", "-target", "1.8", sourceFile);
-        return errors.toString();
-    }
-
     public static void runDx(String[] args) {
         com.android.dx.command.Main.main(args);
     }
 
-    public static void runBaksmali(String[] args) {
-        org.jf.baksmali.Main.main(args);
+    public static void runBaksmali(String dexFilePath) {
+        org.jf.baksmali.Main.main(new String[]{"d", dexFilePath, "-o", new File(dexFilePath).getAbsoluteFile().getParentFile().getAbsolutePath() + Main.SEPARATOR + OUT_SMALI_FOLDER});
     }
 }
